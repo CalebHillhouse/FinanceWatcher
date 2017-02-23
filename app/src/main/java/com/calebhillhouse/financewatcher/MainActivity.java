9086bf42dacbd1f007b8df0eb04ea57d09295efc
Caleb Hillhouse <calebinski94@gmail.com>
2017-02-23 11:22:06 -0600

    Android app called FinanceWatcher

---
+++ app/src/main/java/com/calebhillhouse/financewatcher/MainActivity.java
@@ -0,0 +1,45 @@
+package com.calebhillhouse.financewatcher;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button currentFinances;
+    Button setFinances;
+    Button settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //find buttons
+        currentFinances = (Button)findViewById(R.id.current_finances_button);
+        setFinances = (Button)findViewById(R.id.set_finances_button);
+        //settings = (Button)findViewById(R.id.settings_button);
+
+        //set listener to look for button click
+        currentFinances.setOnClickListener(new View.OnClickListener(){
+            //when you click it...
+            public void onClick(View v){
+                //open new activity (allowances)
+                Intent intent = new Intent(MainActivity.this, CurrentFinanceInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        //set listener to look for button click
+        setFinances.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //open new finances activity
+                Intent intent = new Intent(MainActivity.this, NewFinances.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
